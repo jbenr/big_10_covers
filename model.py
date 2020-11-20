f12c5a0d2bedd2bbec8e0cd1d519009d79dbf977
sammbrad <63823120+sammbrad@users.noreply.github.com>
2020-11-20 15:19:18 -0500

    newChanges

--- model.py
+++ model.py
@@ -1,12 +1,15 @@
 import pandas as pd
+import xlrd
+# Reads excel file as a DataFrame object
 df = pd.read_excel("17_18_games.xls", parse_dates=["Date"])
 
+# This is pulling the first n rows from the DataFrame object
 df.head(2)
 
 # Renaming columns
 df.columns = ["Date", "Visitor", "V_Points", "Home", "H_Points", "OT", "Notes"]
 
-#Prediction class
+# Prediction class
 df["Home Win"] = df["V_Points"] < df["H_Points"]
 
 print("Home Win percentage: {0:.1f}%".format(100 * df["Home Win"].sum() / df["Home Win"].count()))
@@ -28,14 +31,14 @@ for index, row in df.iterrows():
     visitor_team = row["Visitor"]
     row["Home Last Win"] = won_last[home_team]
     row["Visitor Last Win"] = won_last[visitor_team]
-    df.ix[index] = row
-    #We then set our dictionary with the each team's result (from this row) for the next
-    #time we see these teams.
-    #Set current Win
+    df.loc[index] = row     # Changed df.ix[index] to df.loc[index], .ix is deprecated, was getting errors - Sam
+    # We then set our dictionary with the each team's result (from this row) for the next
+    # time we see these teams.
+    # Set current Win
     won_last[home_team] = row["Home Win"]
     won_last[visitor_team] = not row["Home Win"]
 
-#Which team won their last encounter
+# Which team won their last encounter
 df["Home Win Streak"] = 0
 df["Visitor Win Streak"] = 0
 win_streak = defaultdict(int)
@@ -45,7 +48,7 @@ for index, row in df.iterrows():
     visitor_team = row["Visitor"]
     row["Home Win Streak"] = win_streak[home_team]
     row["Visitor Win Streak"] = win_streak[visitor_team]
-    df.ix[index] = row    
+    df.loc[index] = row     # Changed df.ix[index] to df.loc[index], .ix is deprecated, was getting errors - Sam
     # Set current win
     if row["Home Win"]:
         win_streak[home_team] += 1
@@ -56,13 +59,19 @@ for index, row in df.iterrows():
 
 # The standing of the team
 df["Home Ranks Higher"] = 0
-for index , row in df.iterrows():
+for index, row in df.iterrows():
     home_team = row["Home"]
     visitor_team = row["Visitor"]
-    home_rank = standing[standing["School"] == home_team]["Rk"].values[0]
-    visitor_rank = standing[standing["School"] == visitor_team]["Rk"].values[0]
-    row["Home Rank Higher"] = int(home_rank > visitor_rank)
-    df.ix[index] = row
+    indexer = 0
+    for team in standing['Unnamed: 1']:                     # Replaced
+        if home_team == team:                               # home_rank = standing[standing["School"] == home_team]["Rk"].values[0]
+            home_rank = standing['Unnamed: 0'][indexer]     # and visitor_rank = standing[standing["School"] == visitor_team]["Rk"].values[0]
+        elif visitor_team == team:                          # with this for loop
+            visitor_rank = standing['Unnamed: 0'][indexer]  # - Sam
+        indexer += 1                                        #
+    row["Home Ranks Higher"] = int(home_rank > visitor_rank)
+    df.loc[index] = row     # Changed df.ix[index] to df.loc[index], .ix is deprecated, was getting errors - Sam
+
 
 # Which team won their last encounter team regardless of playing at home
 last_match_winner = defaultdict(int)
@@ -73,7 +82,7 @@ for index , row in df.iterrows():
     teams = tuple(sorted([home_team, visitor_team]))
     
     row["Home Won Last"] = 1 if last_match_winner[teams] == row["Home"] else 0
-    df.ix[index] = row
+    df.loc[index] = row         # Changed df.ix[index] to df.loc[index], .ix is deprecated, was getting errors - Sam
     # Who won this one?
     winner = row["Home"] if row["Home Win"] else row["Visitor"]
     last_match_winner[teams] = winner
@@ -93,22 +102,22 @@ print("Accuracy: {0:.1f}%".format(np.mean(scores) * 100))
 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 encoding = LabelEncoder()
-#We will fit this transformer to the home teams so that it learns an integer
-#representation for each team
+# We will fit this transformer to the home teams so that it learns an integer
+# representation for each team
 encoding.fit(df["Home"].values)
 
 home_teams = encoding.transform(df["Home"].values)
 visitor_teams = encoding.transform(df["Visitor"].values)
 X_teams = np.vstack([home_teams, visitor_teams]).T
 
-#OneHotEncoder transformer to encode 
+# OneHotEncoder transformer to encode
 onehot = OneHotEncoder()
-#fit and transform 
+# fit and transform
 X_teams = onehot.fit_transform(X_teams).todense()
 
 X_all = np.hstack([X_features_only, X_teams])
 
-#we run the decision tree on the new dataset
+# we run the decision tree on the new dataset
 clf = DecisionTreeClassifier(random_state=14)
 scores = cross_val_score(clf, X_all, y_true, scoring='accuracy')
 print("Accuracy: {0:.1f}%".format(np.mean(scores) * 100))
@@ -117,35 +126,35 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 import sklearn.metrics
 
-X_small = df[['Home Team Ranks Higher', 'Home Win Streak']]
-pred_train, pred_test, tar_train, tar_test  =   train_test_split(X_small, y_true, test_size=.4)
+X_small = df[['Home Ranks Higher', 'Home Win Streak']]      # was 'Home Team Ranks Higher', changed to correct index 'Home Ranks Higher'  - Sam
+pred_train, pred_test, tar_train, tar_test = train_test_split(X_small, y_true, test_size=.4)
 
-#Build model on training data
-classifier=DecisionTreeClassifier()
-classifier=classifier.fit(pred_train,tar_train)
+# Build model on training data
+classifier = DecisionTreeClassifier()
+classifier = classifier.fit(pred_train, tar_train)
 
-predictions=classifier.predict(pred_test)
+predictions = classifier.predict(pred_test)
 
-sklearn.metrics.confusion_matrix(tar_test,predictions)
+sklearn.metrics.confusion_matrix(tar_test, predictions)
 
 sklearn.metrics.accuracy_score(tar_test, predictions)
 print("Accuracy: {0:.1f}%".format(sklearn.metrics.accuracy_score(tar_test, predictions) * 100))
 
-#Displaying the decision tree
+# Displaying the decision tree
 from sklearn import tree
-#from StringIO import StringIO
+# from StringIO import StringIO
 from io import StringIO
-#from StringIO import StringIO 
+# from StringIO import StringIO
 from IPython.display import Image
 out = StringIO()
 tree.export_graphviz(classifier, out_file=out)
 
 import pydotplus
 graph=pydotplus.graph_from_dot_data(out.getvalue())
-Image(graph.create_png())
+# Image(graph.create_png())
 
 
-#Random forest classifiers
+# Random forest classifiers
 from sklearn.ensemble import RandomForestClassifier
 clf = RandomForestClassifier(random_state=14)
 scores = cross_val_score(clf, X_all, y_true, scoring='accuracy')
@@ -166,7 +175,6 @@ print("Accuracy: {0:.1f}%".format(np.mean(scores) * 100))
 # Give final output
 
 # Sam's Testing
-# from self import self
 
 # from data import *
 
